61a0233e3d3ab243b55ae352db6d1ca8859ea224
Filipe ANTUNES DA SILVA <f.antunesdasilva@student.isartdigital.com>
2019-04-09 13:34:11 -0400

    connected multiple devices on localhost or LAN only ability to send test message

--- UlTrivia_Client/Assets/Scripts/Client.cs
+++ UlTrivia_Client/Assets/Scripts/Client.cs
@@ -1,14 +1,18 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 using UnityEngine.Networking;
 
 public class Client : MonoBehaviour
 {
-    private const int MAX_USER = 4;
-    private const int PORT = 26000;
-    private const int WEB_PORT = 26001;
-    private const string SERVER_IP = "127.0.0.1";
+    private const int    MAX_USER  = 4;
+    private const int    PORT      = 26000;
+    private const int    WEB_PORT  = 26001;
+    private const string SERVER_IP = "10.5.4.53";
+    private const int    BYTE_SIZE = 1024;
 
     private int m_hostId;
+    int         m_connectionId; // Which user is sending ?
 
     private bool m_isStarted;
 
@@ -19,6 +23,48 @@ public class Client : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         Init();
+    }
+
+    private void Update()
+    {
+        UpdateMessagePump();
+    }
+
+    public void UpdateMessagePump()
+    {
+        if (!m_isStarted)
+            return;
+
+        int recHostId; // Web ? Or standalone    (we'll use standalone only)
+        int channelId; // Which lane was it sent from
+
+        byte[] recBuffer = new byte[BYTE_SIZE];
+        int    dataSize;
+
+        NetworkEventType type = NetworkTransport.Receive(out recHostId, out m_connectionId, out channelId, recBuffer,
+                BYTE_SIZE, out dataSize, out m_error);
+
+        switch (type)
+        {
+            case NetworkEventType.Nothing:
+                break;
+
+            case NetworkEventType.ConnectEvent:
+                Debug.Log("Successful connection to the server");
+                break;
+
+            case NetworkEventType.DisconnectEvent:
+                Debug.Log("Client disconnected from the server");
+                break;
+
+            case NetworkEventType.DataEvent:
+                Debug.Log("Data");
+                break;
+
+            case NetworkEventType.BroadcastEvent:
+                Debug.Log("Unexpected network event type");
+                break;
+        }
     }
 
     public void Init()
@@ -34,15 +80,8 @@ public class Client : MonoBehaviour
         // Client only code
         m_hostId = NetworkTransport.AddHost(topo, 0);
 
-#if UNITY_WEBGL && !UNITY_EDITOR
-		// Web Client
-		NetworkTransport.Connect(m_hostId, SERVER_IP, WEB_PORT, 0, out m_error);
-        Debug.Log("Connection from Web");
-#else
-        // Standalone Client
         NetworkTransport.Connect(m_hostId, SERVER_IP, PORT, 0, out m_error);
         Debug.Log("Connection from standalone");
-#endif
 
         Debug.Log(string.Format("Attempting to connect on {0}...", SERVER_IP));
         m_isStarted = true;
@@ -53,4 +92,19 @@ public class Client : MonoBehaviour
         m_isStarted = false;
         NetworkTransport.Shutdown();
     }
+
+#region Send
+    //every class depending on NetMsg will work here as parameter
+    public void SendServer(NetMsg msg)
+    {
+        // Sending buffer and receive buffer need to hold same size !
+        byte[] buffer = new byte[BYTE_SIZE];
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream(buffer);
+        formatter.Serialize(ms, msg);
+        
+        NetworkTransport.Send(m_hostId, m_connectionId, m_reliableChannel, buffer, BYTE_SIZE, out m_error);
+    }
+#endregion
 }
