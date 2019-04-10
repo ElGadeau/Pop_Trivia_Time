6fb244b145c82b26c480ef84a5560226233e85db
Tim <t.cormier@student.isartdigital.com>
2019-04-10 15:14:17 -0400

    plus decrans encore

---
+++ UlTrivia_Client/Assets/Scripts/TimScript/fuseego.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fuseego : MonoBehaviour {
+
+    Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void lancerFusee()
+    {
+        anim.SetTrigger("clicked");
+    }
+}
