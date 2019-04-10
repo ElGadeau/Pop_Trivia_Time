9dc106b9b386d7844205d3b091e3c9da0332b53e
Tim <t.cormier@student.isartdigital.com>
2019-04-10 11:00:54 -0400

    Premier push tim proto

---
+++ UlTrivia_Client/Assets/Scripts/TimScript/countDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class countDown : MonoBehaviour {
+
+    TextMeshProUGUI text; 
+
+    public void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        StartCoroutine(StartCountdown());
+    }
+
+    float currCountdownValue;
+    public IEnumerator StartCountdown(float countdownValue = 30)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue >= 0)
+        {
+            text.text = Mathf.Round(currCountdownValue).ToString();
+            //Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+    }
+}
---
+++ UlTrivia_Client/Assets/Scripts/TimScript/popPin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popPin : MonoBehaviour {
+
+    Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void setPin()
+    {
+        anim.SetTrigger("pin");
+    }
+}
