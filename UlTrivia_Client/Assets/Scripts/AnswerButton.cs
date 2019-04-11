a4377e2110975c2ed8e3a35b19231d124a2fe753
Lilian <lilgado@hotmail.ca>
2019-04-11 14:46:53 -0400

    upgrade clientPlay scene

--- UlTrivia_Client/Assets/Scripts/AnswerButton.cs
+++ UlTrivia_Client/Assets/Scripts/AnswerButton.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 public class AnswerButton : MonoBehaviour
 {
     public TextMeshProUGUI m_textMesh;
+    [SerializeField] private Canvas m_activeCanvas;
+    [SerializeField] private Canvas m_nextCanvas;
+    
     
     public void SendAnswer()
     {
@@ -15,5 +18,14 @@ public class AnswerButton : MonoBehaviour
 
         st.Text = m_textMesh.text;
         Client.m_instance.SendServer(st);
+        StartCoroutine(WaitChangeCanvas());
+    }
+
+    IEnumerator WaitChangeCanvas()
+    {
+        yield return new WaitForSeconds(3.0f);
+        m_activeCanvas.gameObject.SetActive(false);
+        m_nextCanvas.gameObject.SetActive(true);
+        Debug.Log("iwehf");
     }
 }
